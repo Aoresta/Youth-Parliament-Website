094fb4b54ed61622792fba74aaf10050fed3817e
Tejas <tejasbansal1809@gmail.com>
2026-05-10 20:21:00 +0530

    message

--- app/members-edit-here.js
+++ app/members-edit-here.js
@@ -68,7 +68,7 @@ export const tableOfficerMembersEditable = [
   { name: "Vani Arora", role: "Chief Table Officer", image: "/ministers/vani.jpeg", ideology: "If its meant to be, it will be" },
   { name: "Kritika", role: "Chief Table Officer", image: "/ministers/kritika.jpeg", ideology: "Think critically, act responsibly, serve selflessly" },
   { name: "Anshika Yadav", role: "Chief Table Officer", image: "/ministers/anshika.jpeg", ideology: "Accuracy is service" },
-  { name: "Hryan Gola", role: "Chief Table Officer", image: "/ministers/hyran.jpeg", ideology: "Every question deserves order" },
+  { name: "Hryan Gola", role: "Chief Table Officer", image: "/ministers/hryan.jpeg", ideology: "Every question deserves order" },
   { name: "Mayank Shekhar", role: "Helping Hand", image: "/ministers/mayank.jpeg", ideology: "Records protect the House" },
   { name: "Vihaan Mittal", role: "", image: "/ministers/vihaan.jpeg", ideology: "Don't just exist, live it the way you want" },
   { name: "Divyansh Chaudhary", role: "", image: "/ministers/divyansh.jpeg", ideology: "Time discipline is debate discipline" },
